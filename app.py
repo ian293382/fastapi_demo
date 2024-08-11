6ce899ea7d17e0ffab2649ad6c74bf3c2fc51b02
Iannaan <moshou5571228@gmail.com>
2024-08-11 13:43:43 +0800

    use python app.py can start server

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI() # 先創造對象
+
+@app.get('/')
+async def read_root():
+    return {'message': 'This is root address!'}
+
 
 @app.get('/helloworld')
 async def read_root():
     return {'message': 'Hello, World!'}
 
+
+# 修正不要寫 uvicorn helloworld:app -reload
+if __name__ == '__main__':
+    uvicorn.run(app="app:app", reload=True )
